f95de5f798bee05dc7ea9e1b92418543fde667e6
iantan <ian.howell.tan@gmail.com>
2020-07-04 14:00:57 +0200

    Add half card

--- src/main/resources/META-INF/resources/app.js
+++ src/main/resources/META-INF/resources/app.js
@@ -8,7 +8,7 @@ window.init = function () {
 };
 
 function generatePokerDeck() {
-    const pokerCards = [0, 1, 2, 3, 5, 8, 13, 20, 40, 100, '?', 'infinity'];
+    const pokerCards = [0, '½', 1, 2, 3, 5, 8, 13, 20, 40, 100, '?'];
     const card = (item) => html`
         <label class="btn btn-warning" style="padding: 30px; border:5px solid white;">
             <input type="radio" name="selected-card" onclick="scoreIt('${item}')">${item === 'infinity' ? '\u221e' : item}</input>
@@ -45,20 +45,20 @@ function loadResult(scores) {
 // WEBSOCKET
 var socket;
 var connected = false;
-window.connect = function () {
+window.connect = function (varIsPlayer) {
     const user = document.getElementById("user").value;
-    const isPlayer = document.getElementById("isPlayer").checked;
+    const isPlayer = varIsPlayer;
     if (user.length < 1)
         return;
 
     if (!connected) {
         socket = new WebSocket("ws://" + location.host + "/poker-socket/" + user + "/" + isPlayer);
         socket.onopen = function () {
-            console.log("Connected to the web socket");
             connected = true;
             document.getElementById("user").readOnly = true;
-            document.getElementById("isPlayer").disabled = true;
-            document.getElementById("btnConnect").style.display = "none";
+            document.getElementById("lblConnectAs").style.display = "none";
+            document.getElementById("btnPlayer").style.display = "none";
+            document.getElementById("btnDealer").style.display = "none";
             document.getElementById("btnLogout").style.display = "inline";
             document.getElementById("divMain").style.display = "inline";
             document.getElementById("divInitMain").style.display = "none";
@@ -67,7 +67,6 @@ window.connect = function () {
         };
         socket.onmessage = function (m) {
             var resp = JSON.parse(m.data);
-            console.log(resp);
             loadResult(resp.scores);
 
             document.getElementById("btnShowResults").style.visibility = resp.allVotesAreIn ? "visible" : "hidden";
@@ -117,8 +116,9 @@ window.logout = function () {
     }
     areResultsShowing = false;
     document.getElementById("user").readOnly = false;
-    document.getElementById("isPlayer").disabled = false;
-    document.getElementById("btnConnect").style.display = "inline";
+    document.getElementById("lblConnectAs").style.display = "inline";
+    document.getElementById("btnPlayer").style.display = "inline";
+    document.getElementById("btnDealer").style.display = "inline";
     document.getElementById("btnLogout").style.display = "none";
     document.getElementById("divMain").style.display = "none";
     document.getElementById("divInitMain").style.display = "inline";
